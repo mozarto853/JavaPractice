bbd6cb057b25adb3a8acfe69ecc2d60361a8f5e6
ortiz <mozarto853@gmail.com>
2021-02-03 18:29:59 -0500

    Added to the array class in master branch

--- src/BasicJava/Array.java
+++ src/BasicJava/Array.java
@@ -10,10 +10,17 @@ public class Array {
         age[1] = 8;
         age[2] = 10;
 
-        int[]weight = {110,150,105,173,200};            //This is the second way to write it. It allows you to set the size and add as many numbers as you want
-
         System.out.println(age.length);                 //Gives you the size of the array
         System.out.println(age[2]);                  //Gives you the value at index 2
-        System.out.println(Arrays.toString(weight));        //Gives you the whole array without loop(Not usually recomended)
+        System.out.println(Arrays.toString(age));        //Gives you the whole array without loop(Not usually recomended)
+
+
+        int[]weight = {110,150,105,173,200};            //This is the second way to write it. It allows you to set the size and add as many numbers as you want
+
+        for(int i=0; i<age.length;i++){
+            if(age[i] == 90){
+                System.out.println("Found at index " + i);
+            }
+        }
     }
 }
